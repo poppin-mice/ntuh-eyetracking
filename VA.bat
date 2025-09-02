aedafad1e0e41303929f46233775ff969c4bfe33
huangchink <stu101317@gmail.com>
2025-09-02 14:47:14 +0800

    Apply local changes; ignore build artifacts

--- VA.bat
+++ VA.bat
@@ -2,7 +2,7 @@
 cd /d "C:\Users\User\hospital_final"
 
 REM 明確使用「Miniconda」的 activate.bat 來啟用 hospital
-call "%USERPROFILE%\miniconda3\Scripts\activate.bat" hospital
+call "%USERPROFILE%\anaconda3\Scripts\activate.bat" hospital
 
 python VA_center.py
 pause
